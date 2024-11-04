72e8ddf371d5bd57960ac8096ceadd4ff4e8385e
Norman Snagg <norman.snagg@gmail.com>
2024-11-04 04:27:32 -0600

    Update project

--- cdpneighbors.py
+++ cdpneighbors.py
@@ -1,7 +1,6 @@
 #!/user/bin/env python
 from __future__ import absolute_import, division, print_function
 import json
-import my_tools
 import netmiko
 from netmiko.exceptions import NetmikoTimeoutException
 from netmiko.exceptions import NetmikoAuthenticationException
@@ -12,21 +11,6 @@ import signal
 
 # signal.signal(signal.SIGPIPE, signal.SIG_DFL)  # IOError: Broken pipe
 signal.signal(signal.SIGINT, signal.SIG_DFL)   # Keyboardinterrupt: Ctrl-C
-
-'''
-if len(sys.argv) < 3:
-    print('Usage: cmd_runner.py commands.txt devices.json')
-    exit()
-
-# These lines are used when the command file and the devices file will be entered at the cli prompt
-with open(sys.argv[1]) as cmd_file:
-    devices = json.loads(dev_file.read())
-    dev_file.read()
-
-with open(sys.argv[2]) as dev_file:
-    devices = json.loads(dev_file.read())
-    dev_file.read()
-'''
 
 # username, password = my_tools.get_credentials()
 username, password = 'admin', 'python'
@@ -45,7 +29,6 @@ with open('devices.json') as dev_file:
 for device in devices:
     # Print seperator line
     print('~'*79)
-
     # Remove/Add keys as required for connection
     del device['hostname']
     device['username'] = username
